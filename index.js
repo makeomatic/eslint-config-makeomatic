c7b6a488ac7cc48296e86e7590ffaab79f9b356c
AVVS <v@makeomatic.ru>
2018-07-18 09:36:31 -0700

    major: update rules

--- index.js
+++ index.js
@@ -20,9 +20,22 @@ module.exports = {
       exports: 'never',
       functions: 'never',
     }],
-    'no-multi-assign': 0
+    'no-multi-assign': 0,
+    'no-restricted-syntax': ['error', {
+      selector: 'ForInStatement',
+      // eslint-disable-next-line max-len
+      message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+    }, {
+      selector: 'LabeledStatement',
+      message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+    }, {
+      selector: 'WithStatement',
+      message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+    }],
   },
-  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 7
+  },
   plugins: [
     'promise',
     'import'
